1bc5cf1f3ef704f5bb4ba1d3597ad1214819e04f
Jhoel Gonzalez <jhoel834@gmail.com>
2024-11-17 14:01:51 -0400

    Adicionando plugin do ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+    }
 }
