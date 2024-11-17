ed2204b5abbde736ef2b8eb3c1653f9c621ec960
Jhoel Gonzalez <jhoel834@gmail.com>
2024-11-17 13:54:33 -0400

    Adicionando lógica para o Layout do recycleView

--- app/src/main/java/com/jgonzalez/to_do_list_app/MainActivity.kt
+++ app/src/main/java/com/jgonzalez/to_do_list_app/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.jgonzalez.to_do_list_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jgonzalez.to_do_list_app.adapter.TaskAdapter
+import com.jgonzalez.to_do_list_app.databinding.ActivityMainBinding
+import com.jgonzalez.to_do_list_app.model.Task
+import com.jgonzalez.to_do_list_app.viewmodel.TaskViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val taskViewModel: TaskViewModel by viewModels()
+    private lateinit var adapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = TaskAdapter(
+            onEditTask = { task ->
+                Intent(this, TarefasAdd::class.java).also {
+                    startActivity(it)
+                }
+            },
+            onDeleteTask = { task ->
+
+
+                deleteTask(task)
+
+            }
+        )
+
+        binding.recycleView.layoutManager = LinearLayoutManager(this)
+        binding.recycleView.adapter = adapter
+
+        taskViewModel.allTasks.observe(this, Observer { tasks ->
+            tasks?.let { adapter.setTask(it) }
+
+        })
+
+        binding.addFAB.setOnClickListener {
+            Intent(this, TarefasAdd::class.java).also {
+                it.putExtra("EXTRA_TITLE", "valor" )
+                startActivity(it)
+            }
         }
+
+    }
+
+    private fun deleteTask(task: Task) {
+        taskViewModel.delete(task)
+        Toast.makeText(this, "Task deleted successfully", Toast.LENGTH_SHORT).show()
     }
 }
